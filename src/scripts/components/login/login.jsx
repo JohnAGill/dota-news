c35f13a84eec331c6cee192951f0181a097cf8a1
Richard Gill <richard@rgill.co.uk>
2015-12-31 12:10:53 +0000

    fixed indentation issue

--- src/scripts/components/login/login.jsx
+++ src/scripts/components/login/login.jsx
@@ -26,12 +26,12 @@ export default class Login extends Component {
     }
     return(
       <div>
-          <h3>Email</h3>
-          <input onChange={(e) => this.emailInput(e)} className='email-input' value={this.props.email}></input>
-          <h3>Password</h3>
-          <input onChange={(e) => this.passwordInput(e)} type='password' className='password-input'></input>
-          <button onClick={(e) => this.handleClick(e)} className='btn btn-primary'>Sign Up</button>
-          {this.props.errorMessage ? <p>{this.props.errorMessage}</p> : null}
+        <h3>Email</h3>
+        <input onChange={(e) => this.emailInput(e)} className='email-input' value={this.props.email}></input>
+        <h3>Password</h3>
+        <input onChange={(e) => this.passwordInput(e)} type='password' className='password-input'></input>
+        <button onClick={(e) => this.handleClick(e)} className='btn btn-primary'>Sign Up</button>
+        {this.props.errorMessage ? <p>{this.props.errorMessage}</p> : null}
       </div>
     )
   }
