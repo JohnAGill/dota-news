71c496f7f9af40b6522df535489066a7ca6ad544
john <johnagill95@gmil.com>
2016-01-12 10:22:49 +0000

    updated initial state to use moment for startDate and endDate

--- src/scripts/components/tripCreator/tripCreator.jsx
+++ src/scripts/components/tripCreator/tripCreator.jsx
@@ -1,9 +1,11 @@
 import React, {Component} from 'react' // eslint-disable-line no-unused-vars
 import DatePicker from 'react-datepicker'
+import Moment from 'moment'
 
 export default class tripCreator extends Component {
 
   render() {
+    console.log(Moment())
     return(
       <div className='row input-padding'>
         <div className='row'>
@@ -12,11 +14,11 @@ export default class tripCreator extends Component {
         </div>
         <div className='row'>
           <p className='col-md-1'>StartDate</p>
-          <DatePicker placeholder='Select a date' selected={this.props.trip.startDate} onChange={(e) => this.props.updateStartDate(e)} />
+          <DatePicker placeholder='' selected={Moment(this.props.trip.startDate)} onChange={(e) => this.props.updateStartDate(e)} />
         </div>
         <div className='row'>
           <p className='col-md-1'>EndDate</p>
-          <DatePicker placeholder='Select a date' selected={this.props.trip.endDate} onChange={(e) => this.props.updateEndDate(e)} />
+          <DatePicker placeholder='' selected={Moment(this.props.trip.endDate)} onChange={(e) => this.props.updateEndDate(e)} />
         </div>
         <div className='row'>
           <p className='col-md-1'>Comment</p>
