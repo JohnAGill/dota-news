abed5cd43adccad49adaa01d121c952ab0bb02f3
Richard Gill <richard@rgill.co.uk>
2016-01-20 10:12:31 +0100

    Switched to import, seems to fix .default problem

--- src/scripts/config/index.js
+++ src/scripts/config/index.js
@@ -1,19 +1,21 @@
 /* eslint-disable*/
 import _ from 'lodash'
 import defaults from './defaults'
+import productionConfig from './production'
+import developmentConfig from './development'
 
 const environment = process.env.NODE_ENV
 
 const getEnvironment = () => {
   switch (environment) {
     case 'production':
-      return(require('./production'))
+      return(productionConfig)
     case 'development':
-      return(require('./development'))
+      return(developmentConfig)
     default:
-      return(require('./development'))
+      return(developmentConfig)
   }
 }
 
-export const config = _.merge(defaults, getEnvironment().default)
+export const config = _.merge(defaults, getEnvironment())
 export default config
