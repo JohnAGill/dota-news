50c0386e1077da1decc4a74fc21436bff2af3eb4
john <johnagill95@gmil.com>
2016-01-20 17:05:09 +0000

    fixed lambda and moved reverse

--- src/scripts/redux/actions/stories.js
+++ src/scripts/redux/actions/stories.js
@@ -10,8 +10,8 @@ export default {
       dispatch({type: 'LOAD_STORIES_REQUEST'})
       storiesRef.on('value', (snapshot) => {
         const stories = (snapshot.val() ? snapshot.val() : [])
-        const orderedStories = _.sortBy(stories, (story) => { return(story.date) })
-        dispatch({type: 'LOAD_STORIES_SUCCESS', payload: orderedStories.reverse()})
+        const orderedStories = _.sortBy(stories, (story) => (story.date)).reverse()
+        dispatch({type: 'LOAD_STORIES_SUCCESS', payload: orderedStories})
         return (console.log(stories))
       }, (errorObject) => {
         dispatch({type: 'LOAD_STORIES_ERROR', payload: errorObject.code})
