dbbb7d7db12141e4d7b978df1b737931aeb77e7e
john <johnagill95@gmil.com>
2016-01-20 14:22:40 +0000

    added a production id for google analytics and set up config

--- src/scripts/config/production.js
+++ src/scripts/config/production.js
@@ -1,3 +1,4 @@
 export default {
-  firebaseEndpoint: 'https://esports-news.firebaseio.com/dota/stories'
+  firebaseEndpoint: 'https://esports-news.firebaseio.com/dota/stories',
+  googleAnalyticsID: 'UA-72578826-2'
 }
