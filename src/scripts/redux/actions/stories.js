a892d87a72e265b773b728577dc5dfc502665d30
john <johnagill95@gmil.com>
2016-01-20 16:47:54 +0000

    added sortBy to strories action and removed links of no link is found

--- src/scripts/redux/actions/stories.js
+++ src/scripts/redux/actions/stories.js
@@ -1,5 +1,6 @@
 import Firebase from 'firebase'
 import config from '../../config'
+import _ from 'lodash'
 
 const storiesRef = new Firebase(config.firebaseEndpoint)
 
@@ -9,7 +10,8 @@ export default {
       dispatch({type: 'LOAD_STORIES_REQUEST'})
       storiesRef.on('value', (snapshot) => {
         const stories = (snapshot.val() ? snapshot.val() : [])
-        dispatch({type: 'LOAD_STORIES_SUCCESS', payload: stories})
+        const orderedStories = _.sortBy(stories, (story) => { return(story.date) })
+        dispatch({type: 'LOAD_STORIES_SUCCESS', payload: orderedStories.reverse()})
         return (console.log(stories))
       }, (errorObject) => {
         dispatch({type: 'LOAD_STORIES_ERROR', payload: errorObject.code})
