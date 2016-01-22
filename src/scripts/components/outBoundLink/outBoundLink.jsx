a8779775f9e4bbb661d9f908a416259ea769feeb
john <johnagill95@gmil.com>
2016-01-22 17:11:26 +0000

    fixed comments

--- src/scripts/components/outBoundLink/outBoundLink.jsx
+++ src/scripts/components/outBoundLink/outBoundLink.jsx
@@ -1,13 +1,9 @@
 import React, {Component} from 'react' // eslint-disable-line no-unused-vars
 import ga from 'react-ga'
 
-export default class Home extends Component {
+export default class OutBoundLink extends Component {
 
-  redirect(link) {
-    ga.outboundLink({ label: link },
-      () => location.assign(link)
-    )
-  }
+  redirect(link) { ga.outboundLink({ label: link }, () => location.assign(link)) }
 
   render() {
     return (
