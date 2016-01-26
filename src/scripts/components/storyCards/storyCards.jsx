e4589034b58297c019eab4d5fe7203363a754cfb
john <johnagill95@gmil.com>
2016-01-26 14:04:10 +0000

    changed names of classes

--- src/scripts/components/storyCards/storyCards.jsx
+++ src/scripts/components/storyCards/storyCards.jsx
@@ -11,7 +11,7 @@ export default class StoryCards extends Component {
           (
             <div className='card'>
               <h4 className='text-center'>{story.headline}</h4>
-              <div className='links-margin-bottom'>
+              <div className='links-container'>
                 {story.links[0] ? <li><OutBoundLink link={story.links[0].link} source={story.links[0].source} /></li> : null}
                 {story.links[1] ? <li><OutBoundLink link={story.links[1].link} source={story.links[1].source} /></li> : null}
                 {story.links[2] ? <li><OutBoundLink link={story.links[2].link} source={story.links[2].source} /></li> : null}
