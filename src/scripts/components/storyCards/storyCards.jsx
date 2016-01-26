d044f5015809ea7dcac41860812784803f3cb300
john <johnagill95@gmil.com>
2016-01-26 13:45:33 +0000

    changed css on cards

--- src/scripts/components/storyCards/storyCards.jsx
+++ src/scripts/components/storyCards/storyCards.jsx
@@ -10,13 +10,13 @@ export default class StoryCards extends Component {
         {_.map(this.props.stories, (story) =>
           (
             <div className='card'>
-              <ul>
-                <h4>{story.headline}</h4>
+              <h4 className='text-center'>{story.headline}</h4>
+              <div className='links-margin-bottom'>
                 {story.links[0] ? <li><OutBoundLink link={story.links[0].link} source={story.links[0].source} /></li> : null}
                 {story.links[1] ? <li><OutBoundLink link={story.links[1].link} source={story.links[1].source} /></li> : null}
                 {story.links[2] ? <li><OutBoundLink link={story.links[2].link} source={story.links[2].source} /></li> : null}
-                <p className='story-date'>{story.humanReadableDate}</p>
-              </ul>
+              </div>
+              <p className='story-date'>{story.humanReadableDate}</p>
             </div>
           )
         )}
