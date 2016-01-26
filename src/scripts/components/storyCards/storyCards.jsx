8270100ceaf5a03c20a039e7b8737384de141a65
Richard Gill <richard@rgill.co.uk>
2016-01-26 15:46:14 +0100

    Split story component out

--- src/scripts/components/storyCards/storyCards.jsx
+++ src/scripts/components/storyCards/storyCards.jsx
@@ -1,25 +1,13 @@
 import React, {Component} from 'react' // eslint-disable-line no-unused-vars
 import _ from 'lodash'
-import OutBoundLink from '../outBoundLink/outBoundLink'
+import Story from './story'
 
 export default class StoryCards extends Component {
 
   render() {
     return(
       <div className='container'>
-        {_.map(this.props.stories, (story) =>
-          (
-            <div className='card'>
-              <h4 className='text-center'>{story.headline}</h4>
-              <div className='links-container'>
-                {story.links[0] ? <li><OutBoundLink link={story.links[0].link} source={story.links[0].source} /></li> : null}
-                {story.links[1] ? <li><OutBoundLink link={story.links[1].link} source={story.links[1].source} /></li> : null}
-                {story.links[2] ? <li><OutBoundLink link={story.links[2].link} source={story.links[2].source} /></li> : null}
-              </div>
-              <p className='story-date'>{story.humanReadableDate}</p>
-            </div>
-          )
-        )}
+        {_.map(this.props.stories, (story) => <Story story={story} />)}
       </div>
     )
   }
